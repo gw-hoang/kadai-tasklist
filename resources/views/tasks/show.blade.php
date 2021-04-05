79e58b75d52c3091d9bb4496ac4165beaf979471
hoang.nguyen <hoang.nguyen@greenwich.co.jp>
2021-04-05 06:02:48 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のTask詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>Task</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    {{-- Task編集ページへのリンク --}}
+    {!! link_to_route('tasks.edit', 'このTaskを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {{-- Task削除フォーム --}}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+@endsection
